0544d70937035c6d220520c76c4e3a7df20afe28
Mohammad Younes <myounes@gmail.com>
2014-08-18 13:18:12 +0300

    default

---
+++ src/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MixedAuth.Startup))]
+namespace MixedAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
